c752a0e5ac3fad7a7c6f1064f42f0f60b6c8a6fb
WenziLike <SlavaMyraxin@icloud.com>
2021-01-12 10:52:01 +0100

    my Vue Template

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- Header Content -->
+  <!-- body Content -->
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <h1>HELLO</h1>
+        </div>
+      </section>
+    </div>
+  </div>
+  <!-- Footer Content -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
