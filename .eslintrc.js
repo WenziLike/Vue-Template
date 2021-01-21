b6665e62ebfcee2586b150e40560c9a984cf62f7
WenziLike <SlavaMyraxin@icloud.com>
2021-01-21 16:01:40 +0100

    Upload file eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,17 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'eol-last': 0,
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+    'comma-dangle': ['error', 'only-multiline'],
+    semi: ['error', 'always', { omitLastInOneLineBlock: true }],
+    semi: ['error', 'never'],
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'never',
+      asyncArrow: 'always'
+    }],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }]
   }
 }
